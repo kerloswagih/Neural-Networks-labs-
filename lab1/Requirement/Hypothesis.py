9c5f000c59d94736c0fcb2f1461a9ce7d8346343
helana <helananady7@gmail.com>
2026-02-16 21:12:04 +0200

    feat(lab1): implement Matrix Function requirement

--- lab1/Requirement/Hypothesis.py
+++ lab1/Requirement/Hypothesis.py
@@ -1,52 +1,74 @@
 import numpy as np
 
-class HypothesisFunction:
-    def __init__(self, l, m, k):
-        # TODO [1]: Initialize the function's unknown parameters
-        self.l = None
-        self.m = None
-        self.k = None
-        
-        # TODO [2]: Initialize Wh and Wo matrices from a standard normal distribution
-        self.Wh = None
-        self.Wo = None
-        
-        # TODO [3]: Initialize bo and bo column vectors as zero
-        self.bo = None
-        self.bh = None
 
-    def forward(self, x):
+class HypothesisFunction:
+    l: int
+    m: int
+    k: int
+    Wh: np.ndarray
+    Wo: np.ndarray
+    bh: np.ndarray
+    bo: np.ndarray
+
+    def __init__(self, l: int, m: int, k: int) -> None:
+        # TODO [1]: Initialize the function's unknown parameters
+        self.l = l
+        self.m = m
+        self.k = k
+
+        # TODO [2]: Initialize Wh and Wo matrices from a standard normal distribution
+        self.Wh = np.random.normal(size=(m, l))
+        self.Wo = np.random.normal(size=(k, m))
+
+        # TODO [3]: Initialize bo and bo column vectors as zero
+        self.bo = np.zeros((k, 1))
+        self.bh = np.zeros((m, 1))
+
+    def forward(self, x: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
         # Ensure input shape matches input size
-        assert x.shape[0] == self.l, f"Your input must be consistent the value l={self.l}"
-        
+        assert (
+            x.shape[0] == self.l
+        ), f"Your input must be consistent the value l={self.l}"
+
         # TODO [4]: Compute a as mentioned above
-        a = None
-        
+        a = np.einsum("ij,jk->ik", self.Wh, x) + self.bh
+        a = np.tanh(a)
+
         # TODO [5]: Compute output ignoring ReLU
-        y = None
-        
+        y = np.einsum("ij,jk->ik", self.Wo, a) + self.bo
+
         # TODO [6]: Apply ReLU on the output with numpy boolean masking
-        
+        y[y <= 0] = 0
+
         return y, a
 
-    def double_forward(self, x1, x2):
+    def double_forward(self, x1: np.ndarray, x2: np.ndarray) -> np.ndarray:
         # Perform forward function for the two inputs
         y1, _ = self.forward(x1)
         y2, _ = self.forward(x2)
-        
+
         # TODO [7]: Concatenate the two outputs
-        z = None
-        
+        z = np.concatenate((y1, y2))
+
         # TODO [8]: Normalize the concatenated result
-        z_bar = None
-        
+        z_mean = np.mean(z)
+        z_std = np.std(z)
+
+        z_bar = (z - z_mean) / z_std
+
         return z_bar
-    
+
         # TODO [9]: Annotate all initialized variables and functions above
-        
-    def count_params(self):
+
+    def count_params(self) -> int:
         # TODO [10]: Make a lambda function num_params that takes an array z and returns np.prod(z.shape)
-        num_params = None
+        num_params = lambda z: np.prod(z.shape)
         # TODO [11]: return the total number of parameters by summing the function over Wh, Wo, bh, bo
-        total_params = None
+        total_params = (
+            num_params(self.Wh)
+            + num_params(self.Wo)
+            + num_params(self.bh)
+            + num_params(self.bo)
+        )
+
         return total_params
